5e4d0cbc10f860ba85ee3f67fd75a8e4b3ccfbc0
Megan Hezlep <meghez14@gmail.com>
2022-03-03 21:36:53 -0500

    finished assessment

---
+++ gitInfo.js
@@ -0,0 +1,75 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+var gitDefinition = "is a program that allows individuals and teams to track changes to code projects"; 
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+var gitHubDefinition = "is a program where users can collaborate on projects"; 
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+var gitInitDefnition = "initialize a local repository right here in this folder";
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+var gitCloneDefinition = "creates a clone of an existing repository"; 
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+var gitStatusDefinition = "displays what is being tracked by git, and displays the state of the working area"; 
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+var gitAddDefinition = "is adding files to be tracked"; 
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+var gitCommitDefinition = "creates a snapsot of files that are being tracked"; 
+
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+var gitPushDefinition = "is used to upload a local repository to a remote repository"; 
---
+++ sldc.js
@@ -0,0 +1,27 @@
+
+ // In this section you will answer some work through problems related to the Software Development Lifecycle
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Analysis of Requirements
+// Planning
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const AnalysisofRequirements = "This is the first step, in which "
+const planning = "This is the second step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const design = ""
+const Implementation = ""
+const testingAndintegration = ""
+const maintain = ""
